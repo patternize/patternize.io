6ac7012b8a794518c24b136c45608a703975d0c2
Carl <shikang.liu@mail.mcgill.ca>
2020-03-26 21:45:24 -0700

    [Dependencies Update] Upgrade Docusaurus version to alpha48

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,91 +1,107 @@
 module.exports = {
-  title: 'Patternize.io',
-  tagline: 'Patternize Algorithms and Data Structure',
-  url: 'http://patternize.github.io',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Patternize', // Usually your GitHub org/user name.
-  projectName: 'Patternize.io', // Usually your repo name.
+  title: "Patternize.io",
+  tagline: "Patternize Algorithms and Data Structure",
+  url: "http://patternize.github.io",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "Patternize", // Usually your GitHub org/user name.
+  projectName: "Patternize.io", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Patternize.io',
+      title: "Patternize.io",
       logo: {
-        alt: 'Patternize.io',
-        src: 'img/logo.svg',
+        alt: "Patternize.io",
+        src: "img/logo.svg"
       },
       links: [
-          {to: 'docs/Algorithms/Sorting/BubbleSort', label: 'Algorithms', position: 'left'},
-          {to: 'docs/DataStructures/Advanced/VList', label: 'Data Structures', position: 'left'},
-        {to: 'docs/Patterns/Backtracking/README', label: 'Patterns', position: 'left'},
-        {to: 'docs/Language/Java/README', label: 'Language', position: 'left'},
+        {
+          to: "docs/Algorithms/Sorting/BubbleSort",
+          label: "Algorithms",
+          position: "left"
+        },
+        {
+          to: "docs/DataStructures/Advanced/VList",
+          label: "Data Structures",
+          position: "left"
+        },
+        {
+          to: "docs/Patterns/Backtracking/README",
+          label: "Patterns",
+          position: "left"
+        },
+        {
+          to: "docs/Language/Java/README",
+          label: "Language",
+          position: "left"
+        },
         // {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/patternize/patternize.io',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+          href: "https://github.com/patternize/patternize.io",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Links',
+          title: "Links",
           items: [
             {
-              label: 'Docusaurus',
-              href: 'https://docusaurus.io',
+              label: "Docusaurus",
+              href: "https://docusaurus.io"
             },
             {
-              label: 'PyTutor',
-              href: 'http://pythontutor.com'
+              label: "PyTutor",
+              href: "http://pythontutor.com"
             }
-          ],
+          ]
         },
         {
-          title: 'Resources',
+          title: "Resources",
           items: [
             {
-              label: 'Princeton Algorithms 4th Edition',
-              to: 'https://algs4.cs.princeton.edu/home',
+              label: "Princeton Algorithms 4th Edition",
+              to: "https://algs4.cs.princeton.edu/home"
             }
-          ],
+          ]
         },
         {
-          title: 'Follow Author',
+          title: "Follow Author",
           items: [
             {
-              label: 'Carl Liu',
-              href: 'http://www.carlrocks.com',
+              label: "Carl Liu",
+              href: "http://www.carlrocks.com"
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/gazcn007',
+              label: "GitHub",
+              href: "https://github.com/gazcn007"
             }
-          ],
-        },
+          ]
+        }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Carl Liu (gazcn007)`,
-    },
+      copyright: `Copyright © ${new Date().getFullYear()} Carl Liu (gazcn007)`
+    }
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js")
           // editUrl:
           //   'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
   ],
   // for deployments
-  url: 'https://patternize.github.io',
-  baseUrl: '/',
-  projectName: 'patternize.github.io',
-  organizationName: 'patternize'
+  url: "https://patternize.github.io",
+  baseUrl: "/",
+  projectName: "patternize.github.io",
+  organizationName: "patternize"
 };
--- sidebars.js
+++ sidebars.js
@@ -6,121 +6,111 @@
  */
 
 module.exports = {
-
-    'Algorithms': [
+  Algorithms: [
+    {
+      type: "category",
+      label: "Sorting",
+      items: [
+        "Algorithms/Sorting/BubbleSort",
+        "Algorithms/Sorting/OddEvenSort",
+        "Algorithms/Sorting/SelectionSort",
+        "Algorithms/Sorting/InsertionSort",
+        "Algorithms/Sorting/MergeSort"
+      ]
+    }
+  ],
+  "Data Structure": [
+    {
+      type: "category",
+      label: "Basic",
+      items: [
         {
-            type: 'category',
-            label: 'Sorting',
-            items: [
-                'Algorithms/Sorting/BubbleSort',
-                'Algorithms/Sorting/OddEvenSort',
-                'Algorithms/Sorting/SelectionSort',
-                'Algorithms/Sorting/InsertionSort',
-                'Algorithms/Sorting/MergeSort',
-            ]
+          type: "category",
+          label: "Binary Tree",
+          items: ["QuestionBank/Leetcode/LC103"]
         }
-    ],
-    'Data Structure': [
+      ]
+    },
+    {
+      type: "category",
+      label: "Advanced",
+      items: ["DataStructures/Advanced/VList"]
+    }
+  ],
+  Language: [
+    {
+      type: "category",
+      label: "Java Data Structure",
+      items: [
+        "Language/Java/README",
         {
-            type: 'category',
-            label: 'Basic',
-            items: [
-                {
-                    type: 'category',
-                    label: 'Binary Tree',
-                    items: [
-                        "QuestionBank/Leetcode/LC103"
-                    ]
-                }
-            ]
+          type: "category",
+          label: "List",
+          items: [
+            "Language/Java/List/Interface",
+            "Language/Java/List/ArrayList",
+            "Language/Java/List/LinkedList",
+            "Language/Java/List/Stack",
+            "Language/Java/List/Runtime"
+          ]
         },
         {
-            type: 'category',
-            label: 'Advanced',
-            items: ['DataStructures/Advanced/VList']
-        }
-    ],
-    'Language': [
-        {
-            type: 'category',
-            label: 'Java Data Structure',
-            items: [
-                'Language/Java/README',
-                {
-                    type: 'category',
-                    label: 'List',
-                    items: [
-                        'Language/Java/List/Interface',
-                        'Language/Java/List/ArrayList',
-                        'Language/Java/List/LinkedList',
-                        'Language/Java/List/Stack',
-                        'Language/Java/List/Runtime',
-                    ]
-                },
-                {
-                    type: 'category',
-                    label: 'Set',
-                    items: [
-                        'Language/Java/Set/Interface',
-                        'Language/Java/Set/HashSet',
-                        'Language/Java/Set/Runtime'
-                    ]
-                },
-                {
-                    type: 'category',
-                    label: 'Queue/Dequeue',
-                    items: [
-                        'Language/Java/Queue/Interface',
-                        'Language/Java/List/LinkedList',
-                        'Language/Java/Queue/PriorityQueue',
-                        'Language/Java/Queue/Runtime'
-                    ]
-                },
-                {
-                    type: 'category',
-                    label: 'Map',
-                    items: [
-                        'Language/Java/Map/Interface',
-                        'Language/Java/Map/HashMap',
-                        'Language/Java/Map/HashTable',
-                        'Language/Java/Map/TreeMap',
-                        'Language/Java/Map/LinkedHashMap',
-                        'Language/Java/Map/Runtime'
-                    ]
-                }
-            ]
-        }
-    ],
-    'Patterns': [
-        {
-            type: 'category',
-            label: 'Backtracking',
-            items: [
-                'Patterns/Backtracking/README',
-                {
-                    type: 'category',
-                    label: 'Permutation',
-                    items: [
-
-                        'Patterns/Backtracking/Permutation/Permutation'
-                    ]
-                },
-                'Patterns/Backtracking/ExpressionPuzzle',
-            ]
+          type: "category",
+          label: "Set",
+          items: [
+            "Language/Java/Set/Interface",
+            "Language/Java/Set/HashSet",
+            "Language/Java/Set/Runtime"
+          ]
         },
         {
-            type: 'category',
-            label: 'Bit Manipulation',
-            items: [
-                'Patterns/BitManipulation/README'
-            ]
+          type: "category",
+          label: "Queue/Dequeue",
+          items: [
+            "Language/Java/Queue/Interface",
+            "Language/Java/List/LinkedList",
+            "Language/Java/Queue/PriorityQueue",
+            "Language/Java/Queue/Runtime"
+          ]
         },
         {
-            type: 'category',
-            label: 'Graph',
-            items: [
-                'QuestionBank/Leetcode/LC310'
-            ]
+          type: "category",
+          label: "Map",
+          items: [
+            "Language/Java/Map/Interface",
+            "Language/Java/Map/HashMap",
+            "Language/Java/Map/HashTable",
+            "Language/Java/Map/TreeMap",
+            "Language/Java/Map/LinkedHashMap",
+            "Language/Java/Map/Runtime"
+          ]
         }
-    ]
+      ]
+    }
+  ],
+  Patterns: [
+    {
+      type: "category",
+      label: "Backtracking",
+      items: [
+        "Patterns/Backtracking/README",
+        {
+          type: "category",
+          label: "Permutation",
+          items: ["Patterns/Backtracking/Permutation/Permutation"]
+        },
+        "Patterns/Backtracking/ExpressionPuzzle"
+      ]
+    },
+    {
+      type: "category",
+      label: "Bit Manipulation",
+      items: ["Patterns/BitManipulation/README"]
+    },
+    {
+      type: "category",
+      label: "Graph",
+      items: ["QuestionBank/Leetcode/LC310"]
+    }
+  ]
 };
